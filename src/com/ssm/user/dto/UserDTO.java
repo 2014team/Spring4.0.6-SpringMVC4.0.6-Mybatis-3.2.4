9128492a13382f5395d6698c35918a3701f84770
zhuzq <645874679@qq.com>
2017-10-10 10:52:36 +0800

    优化配置

---
+++ src/com/ssm/user/dto/UserDTO.java
@@ -0,0 +1,54 @@
+package com.ssm.user.dto;
+
+import com.ssm.base.domain.BaseDO;
+
+public class UserDTO extends BaseDO {
+
+	private static final long serialVersionUID = 1L;
+	private String name;
+	private String password;
+	private String comeFrom;
+	private String createDate;
+	private String updateDate;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getComeFrom() {
+		return comeFrom;
+	}
+
+	public void setComeFrom(String comeFrom) {
+		this.comeFrom = comeFrom;
+	}
+
+	public String getCreateDate() {
+		return createDate;
+	}
+
+	public void setCreateDate(String createDate) {
+		this.createDate = createDate;
+	}
+
+	public String getUpdateDate() {
+		return updateDate;
+	}
+
+	public void setUpdateDate(String updateDate) {
+		this.updateDate = updateDate;
+	}
+
+}
